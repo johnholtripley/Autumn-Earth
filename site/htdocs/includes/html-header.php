b6f6335459d90089f15caa896e593125d0f7f11e
John Holt Ripley <john.holtripley@gmail.com>
2015-08-19 17:18:17 +0100

    cookie check

--- site/htdocs/includes/html-header.php
+++ site/htdocs/includes/html-header.php
@@ -22,7 +22,7 @@ include($_SERVER['DOCUMENT_ROOT']."/includes/title-tag.php");
 
 <script>
     var cutsTheMustard = false;
-    if('querySelector' in document && 'localStorage' in window && 'addEventListener' in window) {
+    if('querySelector' in document && 'addEventListener' in window) {
      cutsTheMustard = true;
       document.documentElement.className += " js";
      }
