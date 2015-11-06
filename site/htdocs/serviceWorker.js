7dd8798bdb237927559f39ec5b2458e2f64ebb7c
John Holt Ripley <john.holtripley@gmail.com>
2015-11-06 11:40:58 +0000

    added offline page via ServiceWorker

--- site/htdocs/serviceWorker.js
+++ site/htdocs/serviceWorker.js
@@ -1,5 +1,6 @@
-var version = 4;
+var version = 15;
 var cacheVersionRoot = 'localCache';
+var offlineURL = 'offline.html';
 
 var URLsToCache = [
     '/',
@@ -9,35 +10,49 @@ var URLsToCache = [
     '/js/core.min.js'
 ];
 
-function updateCache() {
-    return caches.open(cacheVersionRoot + version)
-        .then(function(cache) {
-            return cache.addAll(URLsToCache);
-        });
-};
-
 self.addEventListener('install', function(event) {
-    event.waitUntil(updateCache());
-});
-
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-        .then(function(response) {
-            return response || fetch(event.request);
+    var offlineRequest = new Request(offlineURL);
+    event.waitUntil(
+        fetch(offlineRequest).then(function(response) {
+            return caches.open(cacheVersionRoot + version).then(function(cache) {
+                return cache.put(offlineRequest, response);
+            }).then(function(cache) {
+                return cache.addAll(URLsToCache);
+            });
         })
     );
 });
 
+self.addEventListener('fetch', function(event) {
+    // see if it's a request for an html page, and check for network errors and serve the offline page if any:
+    if (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html')) {
+        event.respondWith(
+            fetch(event.request).catch(function(e) {
+                return caches.open(cacheVersionRoot + version).then(function(cache) {
+                    return cache.match(offlineURL);
+                });
+            })
+        );
+    } else {
+        // check for cached assets, if not go to the network:
+        event.respondWith(
+            caches.match(event.request)
+            .then(function(response) {
+                return response || fetch(event.request);
+            })
+        );
+    }
+});
+
 self.addEventListener('activate', function(event) {
-  // check for out of date caches and delete them:
-  event.waitUntil(
-      caches.keys().then(function(cacheNames) {
-          return Promise.all(
-              cacheNames.map(function(cacheName) {
-                  if (cacheName == cacheVersionRoot + (version - 1) {
-                          return caches.delete(cacheName);
-                      }
-                  }));
-          }));
-  });
+    // check for out of date caches and delete them:
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheName == cacheVersionRoot + (version - 1)) {
+                        return caches.delete(cacheName);
+                    }
+                }));
+        }));
+});
