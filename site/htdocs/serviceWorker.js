916ae8aa7e10c616e20702d782088f9a531cba66
John Holt Ripley <john.holtripley@gmail.com>
2015-11-06 10:35:21 +0000

    improved ServiceWorker

--- site/htdocs/serviceWorker.js
+++ site/htdocs/serviceWorker.js
@@ -1,96 +1,43 @@
-var offlineFundamentals = [
-  '/',
-   '/css/base.css',
-  '/js/core.min.js'
+var version = 4;
+var cacheVersionRoot = 'localCache';
+
+var URLsToCache = [
+    '/',
+    '/images/autumn-earth.svg',
+    '/css/base.css',
+    '/fonts/ForoLig-webfont.woff2',
+    '/js/core.min.js'
 ];
-var version = 'v1.0.1';
 
+function updateCache() {
+    return caches.open(cacheVersionRoot + version)
+        .then(function(cache) {
+            return cache.addAll(URLsToCache);
+        });
+};
 
-self.addEventListener('install', installer);
-self.addEventListener('activate', activator);
-self.addEventListener('fetch', fetcher);
+self.addEventListener('install', function(event) {
+    event.waitUntil(updateCache());
+});
 
-function installer (e) {
-  self.skipWaiting();
-  e.waitUntil(caches.open(version + 'fundamentals').then(prefill));
-
-  function prefill (cache) {
-    return cache.addAll(offlineFundamentals);
-  }
-}
-
-function activator (e) {
-  if ('clients' in self && clients.claim) { clients.claim(); }
-
-  e.waitUntil(
-    caches.keys().then(function (keys) {
-      return Promise.all(keys
-        .filter(function (key) {
-          return key.indexOf(version) !== 0;
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            return response || fetch(event.request);
         })
-        .map(function (key) {
-          return caches.delete(key);
-        })
-      );
-    })
-  );
-}
+    );
+});
 
-function fetcher (e) {
-  var request = e.request;
-  if (request.method !== 'GET') {
-    e.respondWith(fetch(request)); return;
-  }
-
-  var url = new URL(request.url);
-
-  e.respondWith(caches.match(request).then(queriedCache));
-
-  function queriedCache (cached) {
-    var networked = fetch(request)
-      .then(fetchedFromNetwork, unableToResolve)
-      .catch(unableToResolve);
-    return cached || networked;
-  }
-
-  function fetchedFromNetwork (response) {
-    var clonedResponse = response.clone();
-    caches.open(version + 'pages').then(function add (cache) {
-      cache.put(request, clonedResponse);
-    });
-    return response;
-  }
-
-  function unableToResolve () {
-    var accepts = request.headers.get('Accept');
-    if (url.origin === location.origin && accepts.indexOf('application/json') !== -1) {
-      return offlineView();
-    }
-    if (accepts.indexOf('image') !== -1) {
-      if (url.host === 'www.gravatar.com') {
-        return caches.match(mysteryMan);
-      }
-      return caches.match(rainbows);
-    }
-    if (url.origin === location.origin) {
-      return caches.match('/offline');
-    }
-    return offlineResponse();
-  }
-
-  function offlineView () {
-    var viewModel = {
-      version: env('TAUNUS_VERSION'),
-      model: { action: 'error/offline' }
-    };
-    var options = {
-      status: 200,
-      headers: new Headers({ 'content-type': 'application/json' })
-    };
-    return new Response(JSON.stringify(viewModel), options);
-  }
-
-  function offlineResponse () {
-    return new Response('', { status: 503, statusText: 'Service Unavailable' });
-  }
-}
+self.addEventListener('activate', function(event) {
+  // check for out of date caches and delete them:
+  event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+          return Promise.all(
+              cacheNames.map(function(cacheName) {
+                  if (cacheName == cacheVersionRoot + (version - 1) {
+                          return caches.delete(cacheName);
+                      }
+                  }));
+          }));
+  });
