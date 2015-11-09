3a19f0daa6621f9fbcf86f00acb25702fcbb56d1
John Holt Ripley <john.holtripley@gmail.com>
2015-11-09 11:38:55 +0000

    fixed reference to fall back image

--- site/htdocs/serviceWorker.js
+++ site/htdocs/serviceWorker.js
@@ -1,5 +1,6 @@
 var version = 'v1.0.8';
 var cacheVersionRoot = 'localCache';
+var fallBackImage = '/images/offline-fallback.jpg';
 
 var URLsToCache = [
     '/',
@@ -7,7 +8,7 @@ var URLsToCache = [
     '/css/base.css',
     '/js/core.min.js',
     '/offline.html',
-    '/images/offline-fallback.jpg'
+    fallBackImage
 ];
 
 function updateStaticCache() {
@@ -43,64 +44,53 @@ self.addEventListener('activate', function(event) {
 
 
 
-  self.addEventListener('fetch', function (event) {
-        var request = event.request;
-        // Always fetch non-GET requests from the network
-        if (request.method !== 'GET') {
-            event.respondWith(
-                fetch(request)
-                    .catch(function () {
-                        return caches.match('/offline.html');
-                    })
-            );
-            return;
-        }
-
-        // For HTML requests, try the network first, fall back to the cache, finally the offline page
-        if (request.headers.get('Accept').indexOf('text/html') !== -1) {
-            event.respondWith(
-                fetch(request)
-                    .then(function (response) {
-                        // add a copy of this page in the cache
-                        var copy = response.clone();
-                        caches.open(cacheVersionRoot + version)
-                            .then(function (cache) {
-                                cache.put(request, copy);
-                            });
-                        return response;
-                    })
-                    .catch(function () {
-                        return caches.match(request)
-                            .then(function (response) {
-                                return response || caches.match('/offline.html');
-                            })
-                    })
-            );
-            return;
-        }
-
-        // For non-HTML requests, look in the cache first, fall back to the network
+self.addEventListener('fetch', function(event) {
+    var request = event.request;
+    // Always fetch non-GET requests from the network
+    if (request.method !== 'GET') {
         event.respondWith(
-            caches.match(request)
-                .then(function (response) {
-                    return response || fetch(request)
-                        .catch(function () {
-                            // If the request is for an image, show an offline placeholder
-                             if (request.headers.get('Accept').indexOf('image') !== -1) {
-                        return new Response('<img src="/images/offline-fallback.jpg">', {
-                            headers: {
-                                'Content-Type': 'image/jpeg'
-                            }
-                        });
-                    }
-                        });
-                })
+            fetch(request)
+            .catch(function() {
+                return caches.match('/offline.html');
+            })
         );
-    });
+        return;
+    }
 
+    // For HTML requests, try the network first, fall back to the cache, finally the offline page
+    if (request.headers.get('Accept').indexOf('text/html') !== -1) {
+        event.respondWith(
+            fetch(request)
+            .then(function(response) {
+                // add a copy of this page in the cache
+                var copy = response.clone();
+                caches.open(cacheVersionRoot + version)
+                    .then(function(cache) {
+                        cache.put(request, copy);
+                    });
+                return response;
+            })
+            .catch(function() {
+                return caches.match(request)
+                    .then(function(response) {
+                        return response || caches.match('/offline.html');
+                    })
+            })
+        );
+        return;
+    }
 
-
-
-
-
-
+    // For non-HTML requests, look in the cache first, fall back to the network
+    event.respondWith(
+        caches.match(request)
+        .then(function(response) {
+            return response || fetch(request)
+                .catch(function() {
+                    // If the request is for an image, show an offline placeholder
+                    if (request.headers.get('Accept').indexOf('image') !== -1) {
+                       return caches.match(fallBackImage);
+                    }
+                });
+        })
+    );
+});
