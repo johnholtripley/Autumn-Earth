fd0f355693339a1b9446684fd4aafc6bb7e7d02d
John Holt Ripley <john.holtripley@gmail.com>
2015-12-22 16:50:08 +0000

    added Merriweather and Alegreya fonts

--- site/htdocs/js/src/init.js
+++ site/htdocs/js/src/init.js
@@ -1,12 +1,44 @@
 // font face loader:
 // https://helloanselm.com/2015/using-webfonts-in-2015/
-var observer = new FontFaceObserver("forolight")
-    .check()
-    .then(function() {
-        document.documentElement.className += " fontsLoaded";
+
+
+
+
+    var fontObservers = [];
+    var fontFamilies = {
+        'Merriweather': [
+            {
+                weight: 400
+            },
+            {
+                weight: 400,
+                style: "italic"
+            },
+            {
+                weight: 700
+            }
+        ],
+        
+        'AlegreyaSC': [
+            {
+                weight: 400
+            }
+        ]
+    }
+
+    Object.keys(fontFamilies).forEach(function(family) {
+        fontObservers.push(fontFamilies[family].map(function(config) {
+            return new FontFaceObserver(family, config).check()
+        }));
+    });
+
+    Promise.all(fontObservers)
+        .then(function() {
+             document.documentElement.className += " fontsLoaded";
         // set a cookie as the font should now be in cache and can be displayed immediately subsequently
         createCookie("fontLoaded", "true", 1);
-    });
+        });
+
 
 // service worker:
 if ('serviceWorker' in navigator) {
