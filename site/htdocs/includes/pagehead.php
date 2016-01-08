5ffe8419304e0a74069a3c99c86ee90e8937acc5
John Holt Ripley <john.holtripley@gmail.com>
2016-01-08 12:36:01 +0000

    moved menu toggle into a p for accessibility to move it away from the logo anchor

--- site/htdocs/includes/pagehead.php
+++ site/htdocs/includes/pagehead.php
@@ -1,7 +1,7 @@
 <div id="mastHead" role="banner">
 <div class="row">
 <div class="small-8 wide-3 columns">
-	<a id="menuToggle" class="icon" href="#navigation">Menu</a>
+	<p><a id="menuToggle" class="icon" href="#navigation">Menu</a></p>
 
 
 <a href="/" id="autumnEarth">
