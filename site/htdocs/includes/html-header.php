3e5ce66c109cc0a3350ec4195f151e6de3c8d4be
John Holt Ripley <john.holtripley@gmail.com>
2016-01-18 12:08:26 +0000

    added client hints header

--- site/htdocs/includes/html-header.php
+++ site/htdocs/includes/html-header.php
@@ -33,7 +33,7 @@ include($_SERVER['DOCUMENT_ROOT']."/includes/title-tag.php");
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-
+<meta http-equiv="Accept-CH" content="DPR,Width,Viewport-Width"> 
 <script>
     var cutsTheMustard = false;
     if('querySelector' in document && 'addEventListener' in window) {
