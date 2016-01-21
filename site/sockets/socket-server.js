633ea86321eb0fbdc2b73896411333b3dd649589
John Holt Ripley <john.holtripley@gmail.com>
2016-01-21 13:52:41 +0000

    basic chat app for socket.io

--- site/sockets/socket-server.js
+++ site/sockets/socket-server.js
@@ -1,8 +1,19 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/template.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+    socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
 http.listen(8080, function(){
