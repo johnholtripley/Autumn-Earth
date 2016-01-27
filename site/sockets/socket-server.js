21442d1cdedb5ad64c39897773523a1e2c49ac4f
John Holt Ripley <john.holtripley@gmail.com>
2016-01-27 16:09:50 +0000

    passing messages back and forth

--- site/sockets/socket-server.js
+++ site/sockets/socket-server.js
@@ -16,13 +16,15 @@ app.listen(8080);
 function handler (req, res) {
       res.writeHead(200);
     res.end("<h1>hello</h1>\n");
+    // is this needed? #########
 }
 
 
 var socket = io.listen(app);
 // Add a connect listener
 socket.on('connection', function(client){ 
-  
+  socket.emit('message', 'welcome - you are connected');
+  console.log("socket connect success - listening...");
   // Success!  Now listen to messages to be received
   client.on('message',function(event){ 
     console.log('Received message from client!',event);
