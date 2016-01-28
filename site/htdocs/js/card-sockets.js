c2c1b0c29835d3721c6d6d5bb20c99d5a9c877bd
John Holt Ripley <john.holtripley@gmail.com>
2016-01-28 09:40:16 +0000

    counting connections and sending appropriate message

--- site/htdocs/js/card-sockets.js
+++ site/htdocs/js/card-sockets.js
@@ -1,37 +1,36 @@
-// https://davidwalsh.name/websocket
-// Create SocketIO instance, connect
 /*
-var socket = new io.Socket('www.autumnearth.com',{
-	port: 8080
-});
-socket.connect(); 
 
+TO DO:
+
+have a unique URL for each room - first two connections are players, 3rd or after can only view
+http://socket.io/docs/rooms-and-namespaces/
+
+
+// successfully connecting and sending messages, but firefox reports
+// "Firefox can't establish a connection to the server at wss://www.autumnearth.com:8080/socket.io/?EIO=3&transport=websocket&sid=Dl4Z1Co15rifYXs5AAAB" error
 
 */
 
 
-var socket = io.connect('www.autumnearth.com:8080');
+var socket = io.connect('https://www.autumnearth.com:8080');
 
 console.log('Connected');
 // Add a connect listener
-socket.on('connect',function() {
-	console.log('Client has connected to the server!');
+socket.on('connect', function() {
+    console.log('Client has connected to the server!');
 });
 // Add a connect listener
-socket.on('message',function(data) {
-	console.log('Received a message from the server!',data);
+socket.on('message', function(data) {
+    console.log('Received a message from the server: ', data);
 });
 // Add a disconnect listener
-socket.on('disconnect',function() {
-	console.log('The client has disconnected!');
+socket.on('disconnect', function() {
+    console.log('The client has disconnected!');
 });
 
 // Sends a message to the server via sockets
 function sendMessageToServer(message) {
-	socket.send(message);
+    socket.send(message);
 }
 
 sendMessageToServer("hi - connected in");
-
-// successfully connecting and sending messages, but firefox reports
-// "Firefox can't establish a connection to the server at wss://www.autumnearth.com:8080/socket.io/?EIO=3&transport=websocket&sid=Dl4Z1Co15rifYXs5AAAB" error
--- site/sockets/socket-server.js
+++ site/sockets/socket-server.js
@@ -1,37 +1,48 @@
-
 var fs = require('fs');
 
 var options = {
-  key: fs.readFileSync('/home/autumnearth.com/ssl.key'),
-  cert: fs.readFileSync('/home/autumnearth.com/ssl.cert'),
-  ca: fs.readFileSync('/home/autumnearth.com/ssl.ca')
+    key: fs.readFileSync('/home/autumnearth.com/ssl.key'),
+    cert: fs.readFileSync('/home/autumnearth.com/ssl.cert'),
+    ca: fs.readFileSync('/home/autumnearth.com/ssl.ca')
 };
 
 var app = require('https').createServer(options, handler),
-io = require('socket.io').listen(app);
+    io = require('socket.io').listen(app);
 
 app.listen(8080);
 
-
-function handler (req, res) {
-      res.writeHead(200);
+function handler(req, res) {
+    res.writeHead(200);
     res.end("<h1>hello</h1>\n");
     // is this needed? #########
 }
 
-
 var socket = io.listen(app);
-// Add a connect listener
-socket.on('connection', function(client){ 
-  socket.emit('message', 'welcome - you are connected');
-  console.log("socket connect success - listening...");
-  // Success!  Now listen to messages to be received
-  client.on('message',function(event){ 
-    console.log('Received message from client!',event);
-  });
-  client.on('disconnect',function(){
- //   clearInterval(interval);
-    console.log('Server has disconnected');
-  });
-
+// Add a connect listener:
+socket.on('connection', function(client) {
+    socket.emit('message', 'welcome - you are connected');
+    console.log("socket connect success - listening...");
+    console.log(io.engine.clientsCount + " open connections");
+    // count connections:
+    var activeConnections = io.engine.clientsCount;
+    switch (activeConnections) {
+        case 1:
+            // if 1 - send message to client saying "waiting for opponent"
+            socket.emit('message', 'waiting for opponent...');
+            break;
+        case 2:
+            // if 2 - determine who goes first and send that to both clients
+            socket.emit('message', 'flipping coin...');
+            break;
+        default:
+            // if 3 or more tell client they are in Spectator Mode
+            socket.emit('message', 'you\'re in spectator mode');
+    }
+    // listen to messages to be received
+    client.on('message', function(event) {
+        console.log('Received message from client!', event);
+    });
+    client.on('disconnect', function() {
+        console.log('player has disconnected');
+    });
 });
