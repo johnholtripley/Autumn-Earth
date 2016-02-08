993b61dcf0777e628c214df1aef5714c66d0868a
John Holt Ripley <john.holtripley@gmail.com>
2016-02-08 15:05:19 +0000

    added card game to sitemap

--- site/htdocs/sitemap.php
+++ site/htdocs/sitemap.php
@@ -26,7 +26,7 @@ include($_SERVER['DOCUMENT_ROOT']."/includes/functions.php");
 <url><loc>https://www.autumnearth.com/community/</loc><priority>0.7</priority></url>
 <url><loc>https://www.autumnearth.com/account/join/</loc><priority>0.7</priority></url>
 
-
+<url><loc>https://www.autumnearth.com/card-game/</loc><priority>0.7</priority></url>
 
 
 
