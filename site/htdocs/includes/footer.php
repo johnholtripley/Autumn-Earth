27f79366ba4a7fc0edda67905d45bd3ec175bec8
John Holt Ripley <john.holtripley@gmail.com>
2016-02-12 10:09:36 +0000

    ongoing work

--- site/htdocs/includes/footer.php
+++ site/htdocs/includes/footer.php
@@ -18,7 +18,7 @@ include($_SERVER['DOCUMENT_ROOT']."/includes/search.php");
 </ul></li>
 <li><a href="/auction/">Auction</a></li>
 <li><a href="/mail/">Mail</a></li>
-<li><a href="/chronicle/">News</a></li>
+<li><a href="/chronicle/">The Chronicle</a></li>
 <li><a href="#">Guide</a></li>
 <li><a href="#">The World</a>
 <ul>
