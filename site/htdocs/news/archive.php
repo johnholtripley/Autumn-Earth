9f2b42126c5774699f55825751346d3e7f68d300
John Holt Ripley <john.holtripley@gmail.com>
2016-03-08 15:06:39 +0000

    added pagination to the Chronicle home page

--- site/htdocs/news/archive.php
+++ site/htdocs/news/archive.php
@@ -15,16 +15,35 @@ include($_SERVER['DOCUMENT_ROOT']."/includes/header.php");
 <?php
 
 
+	if(isset($_GET['page'])) {
+		$pagenumber = $_GET['page'];
+		} else {
+		$pagenumber = 1;
+		}
+
+
 $query = "select * from tblNews WHERE status='1' order by timeAdded DESC";
 $result = mysql_query($query) or die ("couldn't execute query");
 
 
+$numberOfEntries = mysql_num_rows($result);
+$resultsperpage = 5;
+$totalpages = ceil($numberOfEntries/$resultsperpage);
+if ($pagenumber > $totalpages) {
+		$pagenumber = 1;
+		}
 
-if (mysql_num_rows($result) > 0) {
+	$startpoint = ($pagenumber - 1) * $resultsperpage;
+		$endpoint = $pagenumber * $resultsperpage;
+		if ($endpoint > $numberOfEntries) {
+		$endpoint = $numberOfEntries;
+		}
 
+if ($numberOfEntries > 0) {
 
+$rowcount = 0;
 while ($row = mysql_fetch_array($result)) {
-
+if (($rowcount>= $startpoint) && ($rowcount<$endpoint)) {
 	extract($row);
 	$timeAdded = strtotime($timeAdded);
 	echo '<p><strong>'.$newsTitle.'</strong> - '.date('jS F Y',$timeAdded);
@@ -35,6 +54,33 @@ echo ' - posted by '.$postedBy;
 }
 	echo '<br />'.$newsSynopsis.' <a href="/chronicle/'.$cleanURL.'/" title="Click for the full article">read more...</a></p>'."\n";
 }
+$rowcount++;
+}
+
+
+
+
+// pagination:
+
+$thispageurl = "/chronicle/";
+
+if($pagenumber>1) {
+	echo '<a href="'.$thispageurl.'page/'.($pagenumber-1).'" title="View page '.($pagenumber-1).'">previous</a> ';
+}
+
+	for($i = 1; $i <= $totalpages; $i++) {
+				if($i == $pagenumber) { 
+				echo $i.' | ';
+				} else {
+				echo '<a href="'.$thispageurl.'page/'.$i.'" title="View page '.$i.'">'.$i.'</a> | ';
+				}
+			} 
+
+
+if($pagenumber<$totalpages) {
+	echo '<a href="'.$thispageurl.'page/'.($pagenumber+1).'" title="View page '.($pagenumber+1).'">next</a> ';
+}
+
 
 } else {
 echo'<p class="Error">No news items found.</p>';
