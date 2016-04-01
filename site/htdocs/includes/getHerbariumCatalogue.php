448738b5301c7730211e011c51af111639712665
John Holt Ripley <john.holtripley@gmail.com>
2016-04-01 10:47:18 +0100

    tidied up layout for Herbarium

--- site/htdocs/includes/getHerbariumCatalogue.php
+++ site/htdocs/includes/getHerbariumCatalogue.php
@@ -30,7 +30,7 @@ $result = mysql_query($query) or die ("couldn't execute query");
 $numberOfEntries = mysql_num_rows($result);
 
 
-$resultsperpage = 13;
+$resultsperpage = 12;
 $totalpages = ceil($numberOfEntries/$resultsperpage);
 if($endpagenumber > $totalpages) {
 $endpagenumber = $totalpages;
@@ -50,7 +50,7 @@ if (($startpagenumber>0) && ($endpagenumber <= $totalpages)) {
 		$htmlOutput = "";
 		$i = 1;
 		if(($numberOfEntries>0) && (isset($isInitialPageRequest))) {
-	echo '<ul id="herbariumCatalogue" class="row medium-2up wide-5up equalHeights paginatedBlock">';
+	echo '<ul id="herbariumCatalogue" class="row medium-2up wide-4up widest-5up equalHeights paginatedBlock">';
 }
 		while ($row = mysql_fetch_array($result)) {
 			if (($rowcount>= $startpoint) && ($rowcount<$endpoint)) {
@@ -61,6 +61,10 @@ $pictureArray = array(150,277);
 if(($i%$resultsperpage == 1) || ($i%$resultsperpage == 8)) {
 $additionalClass=" spotlight";
 $pictureArray = array(300,604);
+}
+if(($i%$resultsperpage == 9) || ($i%$resultsperpage == 10) || ($i%$resultsperpage == 11) || ($i%$resultsperpage == 0)) {
+$additionalClass=" inbetweenRow";
+$pictureArray = array(150,310);
 }
 
 				if(isset($isInitialPageRequest)) {
