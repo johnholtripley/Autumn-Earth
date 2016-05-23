acc13de274038b2ace4e648602d0ff683b0207f6
John Holt Ripley <john.holtripley@gmail.com>
2016-05-23 09:38:05 +0100

    more names added

--- site/htdocs/includes/herbarium/butterfly-name-suffixes.php
+++ site/htdocs/includes/herbarium/butterfly-name-suffixes.php
@@ -1,3 +1,3 @@
 <?php
-$butterflySuffixes = array("fritillary", "monarch", "dart", "mimic", "glory", "monk", "sprite", "nymph", "wisp", "siren", "damsel", "cardinal", "witch", "processionary", "brocade", "sphinx");
+$butterflySuffixes = array("fritillary", "monarch", "dart", "mimic", "glory", "monk", "sprite", "nymph", "wisp", "siren", "damsel", "cardinal", "witch", "processionary", "brocade", "sphinx", "courtesan", "gypsy");
 ?>
