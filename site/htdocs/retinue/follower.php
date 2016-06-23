ccb539f44c7572b58e0eced76cb514c9afb51a2a
John Holt Ripley <john.holtripley@gmail.com>
2016-06-23 12:50:26 +0100

    added follower surnames

--- site/htdocs/retinue/follower.php
+++ site/htdocs/retinue/follower.php
@@ -35,8 +35,10 @@ $showAll = true;
 
 include($_SERVER['DOCUMENT_ROOT']."/retinue/generation/male-first-names.php");
 include($_SERVER['DOCUMENT_ROOT']."/retinue/generation/female-first-names.php");
+include($_SERVER['DOCUMENT_ROOT']."/retinue/generation/surnames.php");
 $possibleMaleFirstNames = sortSequentialSyllables($maleFirstNameSyllables);
 $possibleFemaleFirstNames = sortSequentialSyllables($femaleFirstNameSyllables);
+$possibleSurnames = sortSequentialSyllables($surnamesNameSyllables);
 
 
 
@@ -49,32 +51,72 @@ do {
 	// pick a random start syllable:
 	$firstWord = array_rand($possibleMaleFirstNames);
 	$maleName = $firstWord;
+	//echo $firstWord . " - ";
 	do {
 		$nextSyllable = $possibleMaleFirstNames[$firstWord][mt_rand(0,count($possibleMaleFirstNames[$firstWord])-1)];
+		//echo $nextSyllable . " - ";
 		$maleName .= $nextSyllable;
 		$firstWord = $nextSyllable;
 		$syllableCount ++;
 	} while ($nextSyllable != " ");
-} while (!($syllableCount>=3));
+	//echo " ** ";
+} while (!(($syllableCount>=3) && ($syllableCount<=5)));
 $maleName = ucfirst($maleName);
-
+//echo "<hr>";
 $numberOfSyllablesAvailable = count($possibleFemaleFirstNames);
 do {
 	$syllableCount = 0;
 	// pick a random start syllable:
 	$firstWord = array_rand($possibleFemaleFirstNames);
 	$femaleName = $firstWord;
+	//	echo $firstWord . " - ";
 	do {
 		$nextSyllable = $possibleFemaleFirstNames[$firstWord][mt_rand(0,count($possibleFemaleFirstNames[$firstWord])-1)];
+	//	echo $nextSyllable . " - ";
 		$femaleName .= $nextSyllable;
 		$firstWord = $nextSyllable;
 		$syllableCount ++;
 	} while ($nextSyllable != " ");
-} while (!($syllableCount>=3));
+//	echo " ** ";
+} while (!(($syllableCount>=3) && ($syllableCount<=5)));
 $femaleName = ucfirst($femaleName);
 
-echo "<h2>".$maleName." (male)</h2>";
-echo "<h2>".$femaleName." (female)</h2>";
+
+
+
+
+
+
+$numberOfSyllablesAvailable = count($possibleSurnames);
+do {
+	$syllableCount = 0;
+	// pick a random start syllable:
+	$firstWord = array_rand($possibleSurnames);
+	$surname = $firstWord;
+	//	echo $firstWord . " - ";
+	do {
+		$nextSyllable = $possibleSurnames[$firstWord][mt_rand(0,count($possibleSurnames[$firstWord])-1)];
+	//	echo $nextSyllable . " - ";
+		$surname .= $nextSyllable;
+		$firstWord = $nextSyllable;
+		$syllableCount ++;
+	} while ($nextSyllable != " ");
+//	echo " ** ";
+} while (!(($syllableCount>=3) && ($syllableCount<=5)));
+$surname = ucfirst($surname);
+
+
+
+
+
+
+
+
+
+
+
+echo "<h2>".$maleName." ".$surname." (male)</h2>";
+echo "<h2>".$femaleName." ".$surname." (female)</h2>";
 
 
 
