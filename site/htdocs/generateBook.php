2cde76e637e8218b9865c4400f68856ef0a981c7
John Holt Ripley <john.holtripley@gmail.com>
2016-08-17 15:21:02 +0100

    created Scriptorium

--- site/htdocs/generateBook.php
+++ site/htdocs/generateBook.php
@@ -5,71 +5,54 @@
 // http://www.gutenberg.org/files/42508/42508-h/42508-h.htm
 
 
-$textSource = file_get_contents($_SERVER['DOCUMENT_ROOT']."/includes/library/sources/alices-adventures-in-wonderland-by-lewis-carroll.txt");
+// $textSource = file_get_contents($_SERVER['DOCUMENT_ROOT']."/includes/scriptorium/sources/alices-adventures-in-wonderland-lewis-carroll.txt");
+$textSource = file_get_contents($_SERVER['DOCUMENT_ROOT']."/includes/scriptorium/sources/curious-creatures-in-zoology-john-ashton.txt");
 
 // isolate space and full stops:
 $textSource = str_ireplace(" ", "####", $textSource);
 $textSource = str_ireplace(".", "####.", $textSource);
-
 $textSplit = explode("####", $textSource);
-
- //var_dump($textSplit);
-
 $textLength = count($textSplit);
 $startPhrases = array();
 $markovSequence = array();
 $i=0;
 $isAStartPhrase = true;
 do {
-
-
-// get next 2 words:
+  // get next 2 words:
   $thisSecondOrder = $textSplit[$i]." ".$textSplit[$i+1];
   if($isAStartPhrase) {
-array_push($startPhrases, $thisSecondOrder);
-}
-if (array_key_exists($thisSecondOrder, $markovSequence)) {
-        array_push($markovSequence[($thisSecondOrder)],$textSplit[$i+2]);
-      } else {
-        $markovSequence[$thisSecondOrder] = array($textSplit[$i+2]);
-      }
-
-$isAStartPhrase = false;
-
-if($textSplit[$i] == ".") {
-$isAStartPhrase = true;
-
-}
-
-$i++;
-
+    array_push($startPhrases, $thisSecondOrder);
+  }
+  if (array_key_exists($thisSecondOrder, $markovSequence)) {
+    array_push($markovSequence[($thisSecondOrder)],$textSplit[$i+2]);
+  } else {
+    $markovSequence[$thisSecondOrder] = array($textSplit[$i+2]);
+  }
+  $isAStartPhrase = false;
+  if($textSplit[$i] == ".") {
+    $isAStartPhrase = true;
+  }
+  $i++;
 } while ($i < $textLength-2);
 
-echo "<code><pre>";
 
-//var_dump($startPhrases);
-//echo "<hr>";
-//var_dump($markovSequence);
-echo "</pre></code>";
+$numberOfSentences = 3;
+$numberOfParagraphs = 3;
+for ($i=0;$i<$numberOfParagraphs;$i++) {
+  $thisPair = $startPhrases[mt_rand(0, count($startPhrases) - 1)];
+  $builtSentence = $thisPair;
+  for ($j=0;$j<$numberOfSentences;$j++) {
+    do {
+      // split off second word:
+      $thisPairSplit = explode(" ",$thisPair);
+      $thisWord = $markovSequence[$thisPair][mt_rand(0,count($markovSequence[$thisPair])-1)];
+      $builtSentence .= " ".$thisWord;
+      $thisPair = $thisPairSplit[1]." ".$thisWord;
+    } while ($thisWord != ".");
+  }
+  $builtSentence = str_ireplace(" .", ".", $builtSentence);
+  echo '<p style="font-family:arial,helvetica,sans-serif;font-size:13px;">'.$builtSentence."</p>";
+}
 
-//echo "<hr>";
-
-// pick random start
-$thisPair = $startPhrases[mt_rand(0, count($startPhrases) - 1)];
-
-$builtSentence = $thisPair;
-$i = 0;
-do {
-  // split off second word:
-  $thisPairSplit = explode(" ",$thisPair);
-$thisWord = $markovSequence[$thisPair][mt_rand(0,count($markovSequence[$thisPair])-1)];
-$builtSentence .= " ".$thisWord;
-$thisPair = $thisPairSplit[1]." ".$thisWord;
-$i++;
-} while ($thisWord != ".");
-
-
-$builtSentence = str_ireplace(" .", ".", $builtSentence);
-echo "<h4>".$builtSentence."</h4>";
 
 ?>
--- site/htdocs/includes/navigation/the-world.php
+++ site/htdocs/includes/navigation/the-world.php
@@ -1,5 +1,6 @@
 	<li><a href="#">The Bestiary</a></li>
 	<li><a href="#">The Pantheon</a></li>
+	<li><a href="/scriptorium/">The Scriptorium</a></li>
 	<li><a href="#">Atlas</a></li>
 	<li><a href="#">The Codex</a></li>
 	<li><a href="/herbarium/">The Herbarium</a></li>
