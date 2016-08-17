9d0def3b53f13f8d968babf7e33d43ea363134fd
John Holt Ripley <john.holtripley@gmail.com>
2016-08-17 09:19:42 +0100

    fixed door collision bug

--- site/htdocs/js/src/game-world/core.js
+++ site/htdocs/js/src/game-world/core.js
@@ -156,13 +156,10 @@ function isATerrainCollision(x, y) {
             break;
         case "d":
             // is a door:
+            // make sure it isn't also blocked on the other corner though before starting the transition ##################
             activeDoorX = x;
             activeDoorY = y;
-            console.log("[" + mapTransition + "]");
-            if (mapTransition == "") {
-                mapTransitionCurrentFrames = 1;
-                mapTransition = "out";
-            }
+           
             return 0;
             break;
         default:
@@ -172,6 +169,8 @@ function isATerrainCollision(x, y) {
 }
 
 function checkHeroCollisions() {
+     activeDoorX = -1;
+            activeDoorY = -1;
     // tile collisions:
     if (key[2]) {
         // up
@@ -181,6 +180,12 @@ function checkHeroCollisions() {
             var tileBottomEdge = (tileCollidedWith + 1) * tileW;
             // use the +1 to make sure it's just clear of the collision tile
             hero.y = tileBottomEdge + hero.height / 2 + 1;
+        } else if (activeDoorX != -1) {
+             console.log("[" + mapTransition + "]");
+            if (mapTransition == "") {
+                mapTransitionCurrentFrames = 1;
+                mapTransition = "out";
+            }
         }
     }
     if (key[3]) {
@@ -189,6 +194,12 @@ function checkHeroCollisions() {
             var tileCollidedWith = getTileY(hero.y + hero.height / 2);
             var tileTopEdge = (tileCollidedWith) * tileW;
             hero.y = tileTopEdge - hero.height / 2 - 1;
+        } else if (activeDoorX != -1) {
+             console.log("[" + mapTransition + "]");
+            if (mapTransition == "") {
+                mapTransitionCurrentFrames = 1;
+                mapTransition = "out";
+            }
         }
     }
     if (key[0]) {
@@ -197,6 +208,12 @@ function checkHeroCollisions() {
             var tileCollidedWith = getTileX(hero.x - hero.width / 2);
             var tileRightEdge = (tileCollidedWith + 1) * tileW;
             hero.x = tileRightEdge + hero.width / 2 + 1;
+        } else if (activeDoorX != -1) {
+             console.log("[" + mapTransition + "]");
+            if (mapTransition == "") {
+                mapTransitionCurrentFrames = 1;
+                mapTransition = "out";
+            }
         }
     }
     if (key[1]) {
@@ -205,6 +222,12 @@ function checkHeroCollisions() {
             var tileCollidedWith = getTileX(hero.x + hero.width / 2);
             var tileLeftEdge = (tileCollidedWith) * tileW;
             hero.x = tileLeftEdge - hero.width / 2 - 1;
+        } else if (activeDoorX != -1) {
+             console.log("[" + mapTransition + "]");
+            if (mapTransition == "") {
+                mapTransitionCurrentFrames = 1;
+                mapTransition = "out";
+            }
         }
     }
 
