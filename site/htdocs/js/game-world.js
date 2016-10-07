746a88011904131794f9b3cc23a2cb7e0156c03d
John Holt Ripley <john.holtripley@gmail.com>
2016-10-07 15:45:14 +0100

    trying gradients

--- site/htdocs/js/game-world.js
+++ site/htdocs/js/game-world.js
@@ -1,45 +1,32 @@
 function updateCartographicMiniMap() {
 
     // cartography canvas is 246px wide
-    cartographyUnits = 246 / (mapTilesX * tileW);
+    var cartographyUnits = 246 / (mapTilesX * tileW);
 
-    x = hero.x * cartographyUnits;
-    y = hero.y * cartographyUnits;
+    var x = hero.x * cartographyUnits;
+    var y = hero.y * cartographyUnits;
 
-    innerRadius = 0;
-    outerRadius = 35;
+    var innerRadius = 0;
+    var outerRadius = 35;
 
     var gradient = offScreenCartographyContext.createRadialGradient(x, y, innerRadius, x, y, outerRadius);
-    gradient.addColorStop(0, 'rgb(0,255,255)');
-    gradient.addColorStop(1, 'rgba(0,255,255,0)');
+    gradient.addColorStop(0, 'rgb(255,255,255)');
+    gradient.addColorStop(1, 'rgba(255,255,255,0)');
 
     offScreenCartographyContext.arc(x, y, outerRadius, 0, 2 * Math.PI);
-
     offScreenCartographyContext.fillStyle = gradient;
     offScreenCartographyContext.fill();
 
-
-cartographyContext.clearRect(0, 0, 246, 246);
-
-     cartographyContext.drawImage(offScreenCartographyCanvas, 0, 0);
+    cartographyContext.clearRect(0, 0, 246, 246);
+    cartographyContext.drawImage(offScreenCartographyCanvas, 0, 0);
     cartographyContext.globalCompositeOperation = 'source-in';
-   
     cartographyContext.drawImage(canvasMapImage, 0, 0);
-
-
-
 }
 
-
-
 function initCartographicMap() {
-
-
     canvasMapImage = document.createElement('img');
     canvasMapImage.src = "/generateCartographicMap.php?playerId=" + characterId + "&dungeonName=" + randomDungeonName + "&plotChests=true&requestedMap=" + newMap;
-
     canvasMapImage.onload = function() {
-        
         // new map:
         offScreenCartographyContext.clearRect(0, 0, 246, 246);
         updateCartographicMiniMap();
