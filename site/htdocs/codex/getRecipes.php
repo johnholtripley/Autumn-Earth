6a209abe8d1f52deae24f77d9c3a53b5d1b3b6f1
John Holt Ripley <john.holtripley@gmail.com>
2016-11-16 13:42:52 +0000

    codex recipes added

--- site/htdocs/codex/getRecipes.php
+++ site/htdocs/codex/getRecipes.php
@@ -22,10 +22,111 @@ include($_SERVER['DOCUMENT_ROOT']."/includes/header.php");
 
 if(isset($_GET["profession"])) {
 	$thisProfession = $_GET["profession"];
-} else {
-	echo "<p>Sorry, that profession wasn't found</p>";
+} 
+
+$query = "select * from tblprofessions where cleanurl='".$thisProfession."'";
+$result = mysql_query($query) or die ("recipes failed");
+if(mysql_num_rows($result) == 0) {
+echo "<p>Sorry, that profession wasn't found</p>";
 	 	header("HTTP/1.0 404 Not Found");
+
+} else {
+
+
+	extract(mysql_fetch_array($result));
+
+
+echo '<h2>'.$thisProfession.'</h2>';
+
+$query2 = "SELECT tblrecipes.*, tblinventoryitems.shortname as recipeFallbackName, tblinventoryitems.description as recipeDescriptionFallback FROM tblrecipes INNER JOIN tblinventoryitems on tblrecipes.creates = tblinventoryitems.itemid where tblrecipes.profession='".$professionID."'";
+
+$result2 = mysql_query($query2) or die ("recipes failed");
+
+
+echo '<ul>';
+
+while ($row = mysql_fetch_array($result2)) {
+	extract($row);
+echo '<li>';
+
+echo '<h3>';
+if($recipeName == "") {
+echo $recipeFallbackName;
+} else {
+	echo $recipeName.'<span> (creates '.$recipeFallbackName.')</span>';
 }
+echo '</h3>';
+echo '<p>';
+if($recipeDescription == "") {
+echo $recipeDescriptionFallback;
+} else {
+	echo $recipeDescription;
+}
+echo '</p>';
+// get the ingredients:
+
+$componentNumbers = "";
+$groupItems = [];
+$numberOfGroups = 0;
+$componentsSplit = explode(",", $components);
+for ($i=0;$i<count($componentsSplit);$i++) {
+	if(is_numeric($componentsSplit[$i])) {
+$componentNumbers .= $componentsSplit[$i].",";
+	} else {
+
+		// add group item
+
+$groupQuery = "select tblinventoryitems.group, tblinventoryitems.shortname as groupShortName from tblinventoryitems where tblinventoryitems.group = '".$componentsSplit[$i]."'";
+$result4 = mysql_query($groupQuery) or die ("ingredients group failed");
+$thisGroupNameJoined = "";
+while ($componentsRow = mysql_fetch_array($result4)) {
+	extract($componentsRow);
+	$thisGroupNameJoined .= $groupShortName.", ";
+}
+$thisGroupNameJoined = rtrim($thisGroupNameJoined, ", ");
+
+	// replace last "," with a "or":
+	$lastCommaPos = strrpos($thisGroupNameJoined, ",");
+	$thisGroupNameJoined = substr($thisGroupNameJoined, 0, $lastCommaPos)   ." or".   substr($thisGroupNameJoined, $lastCommaPos+1);
+
+
+array_push($groupItems, $thisGroupNameJoined);
+
+
+	}
+}
+// remove last comma:
+
+$componentNumbers = rtrim($componentNumbers, ",");
+if($componentNumbers != "") {
+$componentsQuery = "select tblinventoryitems.itemid, tblinventoryitems.shortname as innerShortName from tblinventoryitems where tblinventoryitems.itemid in (".$componentNumbers.")";
+$result3 = mysql_query($componentsQuery) or die ("ingredients failed");
+echo '<p>Ingredients:</p><ol>';
+while ($componentsRow = mysql_fetch_array($result3)) {
+	extract($componentsRow);
+echo '<li>';
+echo $innerShortName;
+echo '</li>';
+}
+
+if(count($groupItems >0)) {
+	for ($i=0;$i<count($groupItems);$i++) {
+		echo '<li>'.$groupItems[$i].'</li>';
+	}
+}
+
+echo '</ol>';
+}
+echo '</li>';
+}
+echo '</ul>';
+}
+
+
+
+
+	
+
 
 ?>
 
