d9382652582d8e69ae5affa93648268e2360b7ba
John Holt Ripley <john.holtripley@gmail.com>
2016-12-22 13:29:54 +0000

    added LoS for return

--- site/htdocs/js/src/game-world/core.js
+++ site/htdocs/js/src/game-world/core.js
@@ -652,7 +652,7 @@ function update() {
         timeSinceLastFrameSwap = 0;
         animateFae();
     }
-moveFae();
+    moveFae();
     moveNPCs();
 }
 
@@ -675,28 +675,30 @@ function heroIsInNewTile() {
         if (fae.currentState == "hero") {
             // check it's not recently visited this hotspot:
             if (fae.recentHotspots.indexOf(i) === -1) {
-            if (isInRange(fae.x, fae.y, thisTileCentreX, thisTileCentreY, fae.range)) {
-                if (hasLineOfSight(getTileX(fae.x), getTileX(fae.y), thisHotspot.centreX, thisHotspot.centreY)) {
-                fae.targetX = thisTileCentreX;
-                fae.targetY = thisTileCentreY;
-                // add this to the list of hotspots so it doesn't return to it again and again:
-                fae.recentHotspots.push(i);
-                fae.currentState = "away";
-            }
+                if (isInRange(fae.x, fae.y, thisTileCentreX, thisTileCentreY, fae.range)) {
+                    if (hasLineOfSight(getTileX(fae.x), getTileX(fae.y), thisHotspot.centreX, thisHotspot.centreY)) {
+                        fae.targetX = thisTileCentreX;
+                        fae.targetY = thisTileCentreY;
+                        // add this to the list of hotspots so it doesn't return to it again and again:
+                        fae.recentHotspots.push(i);
+                        fae.currentState = "away";
+                    }
+                }
             }
         }
+    }
+    if (fae.currentState == "wait") {
+        // check if hero has moved far away, and return if so:
+        if (!(isInRange(fae.x, fae.y, hero.x, hero.y, fae.abandonRadius))) {
+            if (hasLineOfSight(getTileX(fae.x), getTileX(fae.y), hero.tileX, hero.tileY)) {
+                fae.currentState = "hero";
+            }
         }
     }
-if (fae.currentState == "wait") {
-    // check if hero has moved far away, and return if so:
-    if (!(isInRange(fae.x, fae.y, hero.x, hero.y, fae.abandonRadius))) {
-        fae.currentState = "hero";
-    }
+
 }
 
 
-
-}
 
 
 
