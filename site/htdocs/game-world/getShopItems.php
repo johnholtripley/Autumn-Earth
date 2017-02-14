bd2c988920131f791ccd52bcc290def90d4f44e0
unknown <john.holtripley@gmail.com>
2017-02-14 17:34:42 +0000

    selecting items for shops by categories

--- site/htdocs/game-world/getShopItems.php
+++ site/htdocs/game-world/getShopItems.php
@@ -4,6 +4,7 @@ include($_SERVER['DOCUMENT_ROOT']."/includes/signalnoise.php");
 include($_SERVER['DOCUMENT_ROOT']."/includes/connect.php");
 
 
+
 $json ='{
 "mapNumber": 3,
 "shops": [
@@ -12,14 +13,16 @@ $json ='{
 	"uniqueItems":[6,19],
 	"shopSpecialism": 2,
 	"categories": [1,2],
-	"size":"large"
+	"size":"large",
+	"currency":"money"
 },
 {
 	"name":"shop #2",
 	"uniqueItems":[],
 	"shopSpecialism": null,
 	"categories": [3],
-	"size":"small"
+	"size":"small",
+	"currency":"money"
 }
 ]
 
@@ -27,6 +30,31 @@ $json ='{
 
 $jsonData = json_decode($json, true);
 
-echo $jsonData['mapNumber'];
-echo $jsonData['shops'][0]["name"];
+
+
+// get colours:
+$coloursQuery = "SELECT * from tblcolours";
+$allColours = [];
+$colourResult = mysql_query($coloursQuery) or die ("recipes failed");
+while ($colourRow = mysql_fetch_array($colourResult)) {
+	extract($colourRow);
+	array_push($allColours, $colourName);
+}
+
+for ($i=0;$i<count($jsonData['shops']);$i++) {
+echo "<h4>".$jsonData['shops'][$i]["name"]."</h4>";
+$query2 = "SELECT tblinventoryitems.* from tblinventoryitems where tblinventoryitems.itemcategories in (".implode(",",$jsonData['shops'][$i]["categories"]).") order by tblinventoryitems.shortname ASC";
+
+$result2 = mysql_query($query2) or die ("recipes failed:".$query2);
+
+while ($row = mysql_fetch_array($result2)) {
+	extract($row);
+	echo $shortname."<br>";
+}
+
+}
+
+
+
+
 ?>
