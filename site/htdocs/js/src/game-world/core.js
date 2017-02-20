3003b48c5f4a2439622a9c306eb3fe0a7469b5b3
John Holt Ripley <john.holtripley@gmail.com>
2017-02-20 17:24:42 +0000

    map shop data

--- site/htdocs/js/src/game-world/core.js
+++ site/htdocs/js/src/game-world/core.js
@@ -265,33 +265,30 @@ function loadProfessionsAndRecipes(recipeIdsToLoad) {
 
 
 function getShopData() {
-
- thisMapShopItemIds = '';
-
-
-shopJSONData = 'shopData={"mapNumber": 3,"shops": [{"name":"shop #1","hash":"zAbCd","uniqueItems":[],"shopSpecialism": 2,"categories": [1,2],"size":"small","currency":"money"},{"name":"shop #2","hash":"3AbCd","uniqueItems":{"14": [{"colour":3},{"colour":7}],"15": [{"colour":1}]},"shopSpecialism": null,"categories": [],"size":"small","currency":"money"}]}';
-
-
-//if no shops:
-// findInventoryItemData();
-// else
-loadShopData(shopJSONData);
+    thisMapShopItemIds = '';
+    if (thisMapData.shops.length == 0) {
+        findInventoryItemData();
+    } else {
+        // thisMapData.shops #########
+        var shopJSONData = 'shopData={"mapNumber": 3,"shops": [{"name":"shop #1","hash":"zAbCd","uniqueItems":[],"shopSpecialism": 2,"categories": [1,2],"size":"small","currency":"money"},{"name":"shop #2","hash":"3AbCd","uniqueItems":{"14": [{"colour":3},{"colour":7}],"15": [{"colour":1}]},"shopSpecialism": null,"categories": [],"size":"small","currency":"money"}]}';
+        loadShopData(shopJSONData);
+    }
 }
 
 
 
 function loadShopData(shopJSONData) {
-// post data with getJSONWithParams function ####
+    // post data with getJSONWithParams function ####
     getJSONWithParams("/game-world/getShopItems.php", shopJSONData, function(data) {
-
-thisMapShopItemIds = data.allItemIds;
-UI.buildShop(data.markup);
+        thisMapShopItemIds = data.allItemIds;
+        UI.buildShop(data.markup);
         findInventoryItemData();
     }, function(status) {
         // try again:
-      loadShopData(shopJSONData);
+        loadShopData(shopJSONData);
     });
 }
+
 
 
 function findInventoryItemData() {
