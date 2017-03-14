1a5099740c87e3ab4095844606ce799aa24b7e5f
John Holt Ripley <john.holtripley@gmail.com>
2017-03-14 16:15:42 +0000

    check for proc content before parsing json

--- site/htdocs/game-world/getMap.php
+++ site/htdocs/game-world/getMap.php
@@ -7,8 +7,20 @@
  $chr = $_GET["chr"];
  $map = $_GET["map"];
  
-
- $mapData = file_get_contents('../data/chr' .  $chr . '/map' . $map . '.json');
 header('Content-Type: application/json');
- echo $mapData;
+ $mapDataFile = file_get_contents('../data/chr' .  $chr . '/map' . $map . '.json');
+ $pos = strrpos($mapDataFile, '##procedural##');
+if ($pos !== false) {
+$mapData = json_decode($mapDataFile, true);
+// check for any procedural elements that need to be added:
+// ############
+echo json_encode($mapData);
+} else {
+    // no procedural content, so don't parse it, just output:
+    echo $mapDataFile;
+}
+
+
+
+ 
 ?>
