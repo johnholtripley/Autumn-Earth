8da80de610f204310a7e52bbc35b1cd5c60871c0
John Holt Ripley <john.holtripley@gmail.com>
2017-03-15 12:56:52 +0000

    procedural NPC started

--- site/htdocs/game-world/getMap.php
+++ site/htdocs/game-world/getMap.php
@@ -33,27 +33,31 @@ if ($pos !== false) {
 
     for($i=0;$i<count($mapData['map']['npcs']); $i++) {
         if($mapData['map']['npcs'][$i]['name'] == "##procedural##") {
-            // create a procedural hen:
+            // create a procedural NPC:
             include_once($_SERVER['DOCUMENT_ROOT']."/includes/replaceImageHue.php");
-            $mapData['map']['npcs'][$i]['name'] = "Hen";
+            $mapData['map']['npcs'][$i]['name'] = "Person";
             $newColour = substr(md5(rand()), 0, 6);
-            replaceHue('npcs/brown-hen.png','281108',$newColour,40);
-            $mapData['map']['npcs'][$i]['src'] = "procedural/brown-hen-".$newColour.".png";
+            replaceHue('npcs/labourer.png','ffbc47',$newColour,40);
+            $mapData['map']['npcs'][$i]['src'] = "procedural/labourer-".$newColour.".png";
             $mapData['map']['npcs'][$i]['width'] = 20;
             $mapData['map']['npcs'][$i]['height'] = 20;
-            $mapData['map']['npcs'][$i]['spriteWidth'] = 31;
-            $mapData['map']['npcs'][$i]['spriteHeight'] = 29;
-            $mapData['map']['npcs'][$i]['centreX'] = 21;
-            $mapData['map']['npcs'][$i]['centreY'] = 24;
+            $mapData['map']['npcs'][$i]['spriteWidth'] = 75;
+            $mapData['map']['npcs'][$i]['spriteHeight'] = 85;
+            $mapData['map']['npcs'][$i]['centreX'] = 56;
+            $mapData['map']['npcs'][$i]['centreY'] = 73;
             $mapData['map']['npcs'][$i]['movement'] = array("-");
-            $mapData['map']['npcs'][$i]['facing'] = 's';
+            $mapData['map']['npcs'][$i]['facing'] = 'e';
             $mapData['map']['npcs'][$i]['walkThroughDoors'] = false;
             $mapData['map']['npcs'][$i]['speed'] = 2;
-            $mapData['map']['npcs'][$i]['speech'] = array(["", "sound", "hen"]);
+            $mapData['map']['npcs'][$i]['speech'] = array(["hi, I'm random/fancy that/procedurally generated and everything/amazing...", ""]);
             $mapData['map']['npcs'][$i]['speechIndex'] = 0;
+            $mapData['map']['npcs'][$i]['cardGameSpeech'] = array("challenge"=>array("Do you play? Fancy a game then?", "play"),"win"=>array("Woo! I won!", ""),"lose"=>array("Ahh, you beat me...", ""),"draw"=>array("Well played", ""));
             $mapData['map']['npcs'][$i]['isCollidable'] = true;
             $mapData['map']['npcs'][$i]['isMoving'] = false;
-            $mapData['map']['npcs'][$i]['animation']["walk"] = array("length"=>"1", "n"=>"0", "e"=>"0", "s"=>"0", "w"=>"0");
+            $mapData['map']['npcs'][$i]['cardSkill'] = 1;
+            $mapData['map']['npcs'][$i]['baseCardPack'] = 0;
+            $mapData['map']['npcs'][$i]['uniqueCards'] = array(rand(1, 5));
+            $mapData['map']['npcs'][$i]['animation']["walk"] = array("length"=>"1", "n"=>"2", "e"=>"3", "s"=>"0", "w"=>"1");
         }
     }
 
