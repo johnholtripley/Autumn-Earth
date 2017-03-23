afecc1fd2ff9d9f968c9f9ab03507378338c3ab8
John Holt Ripley <john.holtripley@gmail.com>
2017-03-23 11:04:30 +0000

    array inserted

--- site/htdocs/js/src/game-world/pathfinding.js
+++ site/htdocs/js/src/game-world/pathfinding.js
@@ -1,23 +1,16 @@
 if (window.Worker) {
     var pathfindingWorker = new Worker('/js/worker-pathfinding.min.js');
     pathfindingWorker.onmessage = function(e) {
-        console.log('Message received from worker');
-        console.log(e.data);
         thisNPCsName = e.data[0];
-        console.log("looking for " + thisNPCsName);
         // find which NPC this is:
         // http://stackoverflow.com/a/16100446/1054212
         var thisNPCsIndex = thisMapData.npcs.map(function(x) {
-            return x.name; }).indexOf(thisNPCsName);
-        console.log("found at " + thisNPCsIndex);
-          
-
-// insert the new path:
-thisMapData.npcs[thisNPCsIndex].movement.splice(thisMapData.npcs[thisNPCsIndex].movementIndex+1, 0, e.data[1]);
-
-          thisMapData.npcs[thisNPCsIndex].facing = thisMapData.npcs[thisNPCsIndex].foundPath[0];
-          thisMapData.npcs[thisNPCsIndex].isMoving = true;
-
+            return x.name;
+        }).indexOf(thisNPCsName);
+        // insert the new path:
+        // http://stackoverflow.com/a/7032717/1054212
+        thisMapData.npcs[thisNPCsIndex].movement.splice.apply(thisMapData.npcs[thisNPCsIndex].movement, [thisMapData.npcs[thisNPCsIndex].movementIndex + 1, 0].concat(e.data[1]));
+        thisMapData.npcs[thisNPCsIndex].facing = thisMapData.npcs[thisNPCsIndex].foundPath[0];
+        thisMapData.npcs[thisNPCsIndex].isMoving = true;
     }
-
 }
