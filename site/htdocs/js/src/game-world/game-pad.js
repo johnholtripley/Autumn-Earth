458315c4eff815aeeb6c5793112e168ddb9c4d2c
John Holt Ripley <john.holtripley@gmail.com>
2017-07-31 15:30:21 +0100

    added shift shoulder for game pad

--- site/htdocs/js/src/game-world/game-pad.js
+++ site/htdocs/js/src/game-world/game-pad.js
@@ -1,9 +1,16 @@
 function checkForGamePadInput() {
     if (Input.hasGamePadSupport) {
-        key[0] = Input.gamePad.axes[1] <= -0.5 // left
-        key[1] = Input.gamePad.axes[1] >= 0.5 // right
-        key[2] = Input.gamePad.axes[2] <= -0.5 // up
-        key[3] = Input.gamePad.axes[2] >= 0.5 // down
-        key[4] = Input.gamePad.buttons[2].value > 0
+        // left:
+        key[0] = Input.gamePad.axes[1] <= -0.5;
+        // right:
+        key[1] = Input.gamePad.axes[1] >= 0.5;
+        // up: 
+        key[2] = Input.gamePad.axes[2] <= -0.5;
+        // down:
+        key[3] = Input.gamePad.axes[2] >= 0.5;
+        // action (X):
+        key[4] = Input.gamePad.buttons[2].value > 0;
+        // shift (right shoulder 1):
+        key[5] = Input.gamePad.buttons[7].value > 0;
     }
 }
