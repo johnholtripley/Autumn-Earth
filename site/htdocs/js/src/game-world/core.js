f3b600f4d0927f9dbf4695708ce87263f02879cb
John Holt Ripley <john.holtripley@gmail.com>
2017-12-04 12:54:47 +0000

    doors working

--- site/htdocs/js/src/game-world/core.js
+++ site/htdocs/js/src/game-world/core.js
@@ -533,8 +533,8 @@ function prepareGame() {
             hero.allPets[hero.activePets[i]].y = getTileCentreCoordY(hero.allPets[hero.activePets[i]].tileY);
             // check these tiles are within the normal grid - if not use the pet in front's z depth:
             if ((hero.allPets[hero.activePets[i]].tileX < 0) || (hero.allPets[hero.activePets[i]].tileY < 0) || (hero.allPets[hero.activePets[i]].tileX >= mapTilesX) || (hero.allPets[hero.activePets[i]].tileY >= mapTilesY)) {
-             //   hero.allPets[hero.activePets[i]].z = hero.allPets[hero.activePets[i - 1]].z;
-             hero.allPets[hero.activePets[i]].z = 0;
+                hero.allPets[hero.activePets[i]].z = hero.allPets[hero.activePets[i - 1]].z;
+             
             } else {
                 hero.allPets[hero.activePets[i]].z = getElevation(hero.allPets[hero.activePets[i]].tileX, hero.allPets[hero.activePets[i]].tileY);
             }
