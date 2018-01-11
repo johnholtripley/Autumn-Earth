857084762584fac0f4f88b5910749988e6261a07
John Holt Ripley <john.holtripley@gmail.com>
2018-01-11 13:37:03 +0000

    panel close button

--- site/htdocs/js/src/game-world/gathering.js
+++ site/htdocs/js/src/game-world/gathering.js
@@ -3,7 +3,6 @@ function checkForRespawns() {
         if (currentActiveInventoryItems[thisMapData.items[i].type].action == "node") {
             if (thisMapData.items[i].state != "active") {
                 console.log("check re-spawn: " + hero.totalGameTimePlayed + "-" + thisMapData.items[i].timeLastHarvested + " (" + (hero.totalGameTimePlayed - thisMapData.items[i].timeLastHarvested) + ") >= " + currentActiveInventoryItems[thisMapData.items[i].type].respawnRate);
-
                 if (hero.totalGameTimePlayed - thisMapData.items[i].timeLastHarvested >= currentActiveInventoryItems[thisMapData.items[i].type].respawnRate) {
                     thisMapData.items[i].state = "active";
                 }
@@ -15,24 +14,19 @@ function checkForRespawns() {
 
 function processGathering() {
     UI.gathering.stability -= 0.25;
-
-
-
     UI.gathering.quality = capValues(UI.gathering.quality, 0, 100);
     UI.gathering.purity = capValues(UI.gathering.purity, 0, 100);
     UI.gathering.stability = capValues(UI.gathering.stability, 0, 100);
-
     // if any of the values are 0:
     if (UI.gathering.quality * UI.gathering.purity * UI.gathering.stability * UI.gathering.quantity == 0) {
         gatheringComplete();
     }
-
     UI.updateGatheringPanel();
 }
 
 function gatheringComplete() {
     if (UI.gathering.stability == 0) {
-        // get nothing
+        UI.showNotification('<p>Resource failed - nothing was gathered</p>');
     } else {
         //  var generatedObject = currentActiveInventoryItems[UI.gathering.node.type];
         var generatedObject = UI.gathering.node.contains[0];
@@ -48,10 +42,9 @@ function gatheringStopped() {
     UI.gathering.node.quantity = UI.gathering.quantity;
     if ((UI.gathering.node.quantity == 0) || (UI.gathering.node.stability == 0)) {
         // reset the node, and its respawn timer:
-        UI.gathering.node.timeLastHarvested = hero.totalGameTimePlayed;
-        UI.gathering.node.state = "inactive";
-        // reset quantity and stability
         UI.gathering.node.stability = UI.gathering.node.maxStability;
         UI.gathering.node.quantity = UI.gathering.node.maxQuantity;
+        UI.gathering.node.timeLastHarvested = hero.totalGameTimePlayed;
+        UI.gathering.node.state = "inactive";
     }
 }
