91af8cb67490cef4e4e20d2bc2a56cf4ead4a941
John Holt Ripley <john.holtripley@gmail.com>
2018-01-12 14:08:22 +0000

    fixed pointer events for inscription books

--- site/htdocs/js/game-world.js
+++ site/htdocs/js/game-world.js
@@ -13,7 +13,8 @@ var soundsToLoad = {
     'lever': '../sounds/lever-NOT_MINE.mp3',
     'keys': '../sounds/keys-NOT_MINE-wow.mp3',
     'unlock': '../sounds/unlock-NOT_MINE-wow.mp3',
-    'gather1': '../sounds/gather-herb-NOT_MINE-wow.mp3'
+    'gather1': '../sounds/gather-herb-NOT_MINE-wow.mp3',
+    'gather4': '../sounds/mining-NOT_MINE-wow.mp3'
 };
 
 
