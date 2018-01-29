bc65870cdbbe3f2db32776fe20d1f1f2d2ad7822
John Holt Ripley <john.holtripley@gmail.com>
2018-01-29 12:06:51 +0000

    journal

--- site/htdocs/game-world/getQuestJournalEntries.php
+++ site/htdocs/game-world/getQuestJournalEntries.php
@@ -5,8 +5,59 @@ include($_SERVER['DOCUMENT_ROOT']."/includes/connect.php");
 
  // if no quest id is passed in, then get all active
 
- $markupToOutput = '<ol><li>one</li><li>two</li></ol>';
-$regions = array("Teldrassil","Kalimdor");
+$activeQuests = array(2,4,8);
+
+
+
+
+$regions = array();
+$markupToOutput = '<ol>';
+$query = "SELECT tblquests.questid, tblquests.journaltitle, tblquests.journaldesc, tblquests.questregion, tblquests.itemsreceivedoncompletion, tblquests.titlegainedaftercompletion, tbltitles.titleName FROM tblquests left join tbltitles ON tblquests.titlegainedaftercompletion = tbltitles.titleid WHERE questid in (".implode(",",$activeQuests).")";
+
+$result = mysql_query($query) or die ();
+while ($row = mysql_fetch_array($result)) {
+    extract($row);
+    $markupToOutput .= '<li data-region="'.$questregion.'">';
+    $markupToOutput .= '<h4>'.$journaltitle.'</h4>';
+    $markupToOutput .= '<p>'.$journaldesc.'</p>';
+    $markupToOutput .= '<h5>Rewards</h5>';
+
+    // build reward items:
+
+    // check for "/" for random ####
+    // check for $ for money ####
+  $allRewards = explode(",",$itemsreceivedoncompletion);
+  foreach ($allRewards as $item) {
+    $markupToOutput .= '<img src="/images/game-world/inventory-items/'.$item.'.png">';
+  }
+
+/*
+$markupToOutput .= '<img src="/images/game-world/inventory-items/'.$inventoryDataToSort[$j]['itemID'].$colourSuffix.'.png" '.$imgDataAttributes.' alt="'.$inventoryDataToSort[$j]['colourName'].$inventoryDataToSort[$j]['shortname'].'">';
+$markupToOutput .= '<p><em>'.$inventoryDataToSort[$j]['colourName'].$inventoryDataToSort[$j]['shortname'].'</em>';
+$markupToOutput .= '<span class="price'.$specialPriceClass.'">Buy price: '.parseMoney($thisItemsPrice).'</span></p>';
+*/
+
+
+
+if($titlegainedaftercompletion != null) {
+$markupToOutput .= '<p>"'.$titleName.'" title</p>';
+}
+
+    $markupToOutput .= '</li>';
+    if (!in_array($questregion, $regions)) {
+    array_push($regions, $questregion);
+}
+}
+
+mysql_free_result($result);
+
+
+
+
+
+$markupToOutput .= "</ol>";
+
+
 
 // create JSON response:
 echo '{"markup": ["'.addcslashes($markupToOutput, '"\\/').'"],"regions": '.json_encode($regions).'}';
