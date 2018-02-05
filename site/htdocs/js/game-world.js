df0ea8c00ff348bebc9d23b048f446c420c66008
John Holt Ripley <john.holtripley@gmail.com>
2018-02-05 08:54:36 +0000

    fix gamepad chrome issue

--- site/htdocs/js/game-world.js
+++ site/htdocs/js/game-world.js
@@ -827,21 +827,23 @@ function moveFaeToDestination(x, y) {
 
 function checkForGamePadInput() {
     if (Input.isUsingGamePad) {
-        // check if an update has happened since the last one that was acted on:
-        if (Input.gamePad.timestamp != Input.gameLastPadTimeStamp) {
-            Input.gameLastPadTimeStamp = Input.gamePad.timestamp;
-            // left:
-            key[0] = Input.gamePad.axes[1] <= -0.5;
-            // right:
-            key[1] = Input.gamePad.axes[1] >= 0.5;
-            // up: 
-            key[2] = Input.gamePad.axes[2] <= -0.5;
-            // down:
-            key[3] = Input.gamePad.axes[2] >= 0.5;
-            // action (X):
-            key[4] = Input.gamePad.buttons[2].value > 0;
-            // shift (right shoulder 1):
-            key[5] = Input.gamePad.buttons[7].value > 0;
+        if (typeof Input.gamePad.timestamp !== "undefined") {
+            // check if an update has happened since the last one that was acted on:
+            if (Input.gamePad.timestamp != Input.gameLastPadTimeStamp) {
+                Input.gameLastPadTimeStamp = Input.gamePad.timestamp;
+                // left:
+                key[0] = Input.gamePad.axes[1] <= -0.5;
+                // right:
+                key[1] = Input.gamePad.axes[1] >= 0.5;
+                // up: 
+                key[2] = Input.gamePad.axes[2] <= -0.5;
+                // down:
+                key[3] = Input.gamePad.axes[2] >= 0.5;
+                // action (X):
+                key[4] = Input.gamePad.buttons[2].value > 0;
+                // shift (right shoulder 1):
+                key[5] = Input.gamePad.buttons[7].value > 0;
+            }
         }
     }
 }
