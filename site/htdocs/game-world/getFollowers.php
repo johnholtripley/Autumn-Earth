52d594471489abda1bfd8696918461c1fa26f152
John Holt Ripley <john.holtripley@gmail.com>
2018-02-16 12:43:31 +0000

    started retinue

--- site/htdocs/game-world/getFollowers.php
+++ site/htdocs/game-world/getFollowers.php
@@ -8,11 +8,12 @@ include($_SERVER['DOCUMENT_ROOT']."/includes/connect.php");
 // get from game state: ####
 $chr = 999;
 
-
+$activeQuestsIds = array();
   $query = "SELECT * FROM tblretinuefollowers left join tblretinuequests on tblretinuefollowers.activeQuestId = tblretinuequests.questID where tblretinuefollowers.characterIdFollowing='".$chr."'";
 
-      $result = mysql_query($query) or die ("can't get followers");
+      $result = mysql_query($query) or die ();
       if(mysql_num_rows($result)>0) {
+      	echo '<h2>Your retinue:</h2>';
      echo "<ol>";
       while ($row = mysql_fetch_array($result)) {
       extract($row);
@@ -25,6 +26,17 @@ echo '<p>waiting for a quest</p>';
 $completedSoFar = floor((time() - strtotime($questStartedTime))/60);
 if($completedSoFar >= $questTimeRequired ) {
 echo '<p>COMPLETED "'.$questName.'"</p>';
+
+$rewardObject = json_decode($questReward);
+		foreach ($rewardObject as &$thisReward) {
+			if($thisReward->type == "$") {
+$inventoryImage = 'coins';
+			} else {
+$inventoryImage = $thisReward->type;
+			}
+echo '<div class="postSlot"><img src="/images/game-world/inventory-items/'.$inventoryImage.'.png"><span class="qty">'.$thisReward->quantity.'</span></div>';
+}
+
 } else {
 echo '<p>active on "'.$questName.'"" - '.$completedSoFar.' minutes out of '.$questTimeRequired;
 }
@@ -36,6 +48,22 @@ echo '<p>active on "'.$questName.'"" - '.$completedSoFar.' minutes out of '.$que
       mysql_free_result($result);
 
 
+
+
+// get a pool of the latest available quests (all latest quests, that aren;t active or completed by this character)
+      $questsQuery = "SELECT * from tblretinuequests where tblretinuequests.questID NOT IN (SELECT questIdActiveOrComplete from tblretinuequestsactive where characterId='".$chr."') order by timeCreated DESC limit 12";
+
+$questsResult = mysql_query($questsQuery) or die ();
+if(mysql_num_rows($questsResult)>0) {
+	echo'<h2>Available quests:</h2>';
+	echo "<ol>";
+	while ($questsRow = mysql_fetch_array($questsResult)) {
+      extract($questsRow);
+echo '<li><h3>'.$questName.'</h3><p>'.$questDescription.' (requires '.$questNumberOfFollowersRequired.')</p></li>';
+  }
+  echo "</ol>";
+}
+mysql_free_result($questsResult);
 ?>
 
 
