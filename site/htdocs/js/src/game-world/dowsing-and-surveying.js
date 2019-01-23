4e96f7f53f04e93abac2d1a04ecaf25040c1ea56
John Holt Ripley <john.holtripley@gmail.com>
2019-01-23 21:20:42 +0000

    hotspot global coords

--- site/htdocs/js/src/game-world/dowsing-and-surveying.js
+++ site/htdocs/js/src/game-world/dowsing-and-surveying.js
@@ -1,21 +1,24 @@
 function processDowsing() {
-    if (thisMapData.hiddenResources[dowsing.category]) {
-        var closestDistance = Infinity;
-        var thisDistance;
-        var whichIndex = -1;
-        // find the nearest node - of the correct type and react to that:
-        for (var i = 0; i < thisMapData.hiddenResources[dowsing.category].length; i++) {
-            thisDistance = getPythagorasDistance(hero.tileX, hero.tileY, thisMapData.hiddenResources[dowsing.category][i].tileX, thisMapData.hiddenResources[dowsing.category][i].tileY);
-            if (thisDistance < closestDistance) {
-                closestDistance = thisDistance;
-                whichIndex = i;
+    for (var m = 0; m < visibleMaps.length; m++) {
+        whichVisibleMap = visibleMaps[m];
+        if (thisMapData[whichVisibleMap].hiddenResources[dowsing.category]) {
+            var closestDistance = Infinity;
+            var thisDistance;
+            var whichIndex = -1;
+            // find the nearest node - of the correct type and react to that:
+            for (var i = 0; i < thisMapData[whichVisibleMap].hiddenResources[dowsing.category].length; i++) {
+                thisDistance = getPythagorasDistance(hero.tileX, hero.tileY, thisMapData[whichVisibleMap].hiddenResources[dowsing.category][i].tileX, thisMapData[whichVisibleMap].hiddenResources[dowsing.category][i].tileY);
+                if (thisDistance < closestDistance) {
+                    closestDistance = thisDistance;
+                    whichIndex = i;
+                }
             }
-        }
-        if (whichIndex != -1) {
-            dowsing.proximity = 100 - (100 * ((closestDistance) / dowsing.range));
-            dowsing.proximity = capValues(dowsing.proximity, 0, 100);
-        } else {
-            dowsing.proximity = 0;
+            if (whichIndex != -1) {
+                dowsing.proximity = 100 - (100 * ((closestDistance) / dowsing.range));
+                dowsing.proximity = capValues(dowsing.proximity, 0, 100);
+            } else {
+                dowsing.proximity = 0;
+            }
         }
     }
 }
@@ -32,45 +35,48 @@ function processSurveying() {
 function surveyingComplete() {
     var thisDistance, thisResource, sourceTileX, sourceTileY, tryFacing, facingsRemaining;
     var resourceFound = false;
-    if (thisMapData.hiddenResources[surveying.category]) {
-        for (var i = 0; i < thisMapData.hiddenResources[surveying.category].length; i++) {
-            thisResource = thisMapData.hiddenResources[surveying.category][i];
-            thisDistance = getPythagorasDistance(hero.tileX, hero.tileY, thisResource.tileX, thisResource.tileY);
-            if (thisDistance < 2) {
-                tryFacing = hero.facing;
-                switch (tryFacing) {
-                    case 'n':
-                        facingsRemaining = ['e', 'w', 's'];
-                        break;
-                    case 'e':
-                        facingsRemaining = ['n', 's', 'w'];
-                        break;
-                    case 's':
-                        facingsRemaining = ['n', 's', 'e'];
-                        break;
-                    case 'w':
-                        facingsRemaining = ['e', 'w', 'n'];
-                        break;
-                }
-                do {
-                    sourceTileX = hero.tileX + relativeFacing[tryFacing]["x"];
-                    sourceTileY = hero.tileY + relativeFacing[tryFacing]["y"];
-                    tryFacing = facingsRemaining.shift();
-                } while (!tileIsClear(sourceTileX, sourceTileY) && (facingsRemaining.length > 0));
-                if (facingsRemaining.length > 0) {
-                    thisResource.tileX = sourceTileX;
-                    thisResource.tileY = sourceTileY;
-                    thisResource.isTemporary = true;
-                    thisMapData.items.push(thisResource);
-                    initialiseItem(thisMapData.items.length - 1);
-                    resourceFound = true;
-                    // remove it from the list now the node has been generated:
+    for (var m = 0; m < visibleMaps.length; m++) {
+        whichVisibleMap = visibleMaps[m];
+        if (thisMapData[whichVisibleMap].hiddenResources[surveying.category]) {
+            for (var i = 0; i < thisMapData[whichVisibleMap].hiddenResources[surveying.category].length; i++) {
+                thisResource = thisMapData[whichVisibleMap].hiddenResources[surveying.category][i];
+                thisDistance = getPythagorasDistance(hero.tileX, hero.tileY, thisResource.tileX, thisResource.tileY);
+                if (thisDistance < 2) {
+                    tryFacing = hero.facing;
+                    switch (tryFacing) {
+                        case 'n':
+                            facingsRemaining = ['e', 'w', 's'];
+                            break;
+                        case 'e':
+                            facingsRemaining = ['n', 's', 'w'];
+                            break;
+                        case 's':
+                            facingsRemaining = ['n', 's', 'e'];
+                            break;
+                        case 'w':
+                            facingsRemaining = ['e', 'w', 'n'];
+                            break;
+                    }
+                    do {
+                        sourceTileX = hero.tileX + relativeFacing[tryFacing]["x"];
+                        sourceTileY = hero.tileY + relativeFacing[tryFacing]["y"];
+                        tryFacing = facingsRemaining.shift();
+                    } while (!tileIsClear(sourceTileX, sourceTileY) && (facingsRemaining.length > 0));
+                    if (facingsRemaining.length > 0) {
+                        thisResource.tileX = sourceTileX;
+                        thisResource.tileY = sourceTileY;
+                        thisResource.isTemporary = true;
+                        thisMapData[whichVisibleMap].items.push(thisResource);
+                        initialiseItem(thisMapData[whichVisibleMap].items[thisMapData[whichVisibleMap].items.length - 1]);
+                        resourceFound = true;
+                        // remove it from the list now the node has been generated:
 
-                    thisMapData.hiddenResources[surveying.category].splice(i, 1);
-                } else {
-                    console.log("Error - Couldn't place resource node");
+                        thisMapData[whichVisibleMap].hiddenResources[surveying.category].splice(i, 1);
+                    } else {
+                        console.log("Error - Couldn't place resource node");
+                    }
+                    break;
                 }
-                break;
             }
         }
     }
