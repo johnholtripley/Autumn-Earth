ab4c4976e6fc5b10dfe7d6751bf3a385497e9153
John Holt Ripley <john.holtripley@gmail.com>
2019-09-11 14:02:48 +0100

    housing tiles

---
+++ site/htdocs/game-world/getHousingTiles.php
@@ -0,0 +1,29 @@
+<?php
+
+include_once($_SERVER['DOCUMENT_ROOT']."/includes/signalnoise.php");
+include_once($_SERVER['DOCUMENT_ROOT']."/includes/connect.php");
+include_once($_SERVER['DOCUMENT_ROOT']."/includes/functions.php");
+
+
+
+
+// 10 is the Item Category for housing tiles
+$query = 'select * from tblinventoryitems where itemCategories="10" ORDER BY "itemID" ASC';
+
+$htmlOutput = '<ul id="housingTileSelection">';
+
+$result = mysqli_query($connection, $query);
+if(mysqli_num_rows($result)>0) {
+	while ($row = mysqli_fetch_array($result)) {
+		extract($row);
+		$htmlOutput .= '<li data-cost="'.$priceCode.'" data-name="'.$shortname.'"><img src="/images/game-world/items/'.$cleanURL.'.png" alt="'.$shortname.'">';
+		$htmlOutput .= '<p>'.$shortname.' - '.parseMoney($priceCode).'</p></li>';
+	}
+}
+mysqli_free_result($result);
+
+$htmlOutput .= '</ul>';
+
+echo $htmlOutput;
+
+?>
--- site/htdocs/game-world/index.php
+++ site/htdocs/game-world/index.php
@@ -274,6 +274,7 @@ $theseCatalogues = json_decode($catalogues, true);
       <div class="draggableBar">Housing</div>
       <button class="closePanel">close</button>
       <label for="showHousingFootprintCheckbox">Show plot footprint?</label><input type="checkbox" checked="checked" id="showHousingFootprintCheckbox" class="switch">
+      <?php include($_SERVER['DOCUMENT_ROOT']."/game-world/getHousingTiles.php"); ?>
     </div>
 
     <div id="gameSettings"><button class="closePanel">close</button>
