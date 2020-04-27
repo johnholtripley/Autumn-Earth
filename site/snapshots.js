269b274830bb8ef46608852d38404bae6830bb5e
John Holt Ripley <john.holtripley@gmail.com>
2020-04-27 14:57:33 +0100

    circle CLI YML change

--- site/snapshots.js
+++
@@ -1,9 +0,0 @@
-// https://docs.percy.io/docs/percyscript
-const PercyScript = require('@percy/script');
-
-PercyScript.run(async (page, percySnapshot) => {
-  await page.goto('https://www.autumnearth.com/');
-  // ensure the page has loaded before capturing a snapshot
-  await page.waitFor('#footer');
-  await percySnapshot('homepage', { widths: [420, 768, 980, 1200] });
-});
