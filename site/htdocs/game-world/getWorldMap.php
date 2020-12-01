e86a05beb309c6d7576a8b342edb5bcf946856c2
John Holt Ripley <john.holtripley@gmail.com>
2020-12-01 17:21:14 +0000

    global platform global coords

--- site/htdocs/game-world/getWorldMap.php
+++ site/htdocs/game-world/getWorldMap.php
@@ -9,11 +9,21 @@ $worldMapTileLength = 50;
 if(isset($mapJson['globalPlatforms'])) {
     foreach ($mapJson['globalPlatforms'] as $map => $value) {
         $globalPosition = findWorldMapPosition($value['startMap']);
-      //  $mapJson['globalPlatforms'][$map]['startX'] += $globalPosition[0] * $worldMapTileLength;
-      //  $mapJson['globalPlatforms'][$map]['startY'] += $globalPosition[1] * $worldMapTileLength;
-    // just do start and end doors
+        if(isset($mapJson['globalPlatforms'][$map]['startDoors'])) {
+            foreach ($mapJson['globalPlatforms'][$map]['startDoors'] as $innerKey => $innerValue) {
+                $mapJson['globalPlatforms'][$map]['startDoors'][$innerKey]['startX'] += $globalPosition[0] * $worldMapTileLength;
+                $mapJson['globalPlatforms'][$map]['startDoors'][$innerKey]['startY'] += $globalPosition[1] * $worldMapTileLength;
+            }
+        }
+        $globalPosition = findWorldMapPosition($value['endMap']);
+        if(isset($mapJson['globalPlatforms'][$map]['endDoors'])) {
+            foreach ($mapJson['globalPlatforms'][$map]['endDoors'] as $innerKey => $innerValue) {
+                $mapJson['globalPlatforms'][$map]['endDoors'][$innerKey]['startX'] += $globalPosition[0] * $worldMapTileLength;
+                $mapJson['globalPlatforms'][$map]['endDoors'][$innerKey]['startY'] += $globalPosition[1] * $worldMapTileLength;
+            }
+        }
     }
-    }
+}
 echo json_encode($mapJson);
 
 ?>
